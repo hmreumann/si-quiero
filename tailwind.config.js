88265d0100a6134a1217518b3362a4249e0275bf
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-11-06 17:39:44 -0300

    added church and party

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+        fontFamily: {
+            libre: ['"Xanh Mono"'],
+            cormoran: ['"Cormorant Garamond"']
+        }
+    },
+  },
+  plugins: [],
+}
